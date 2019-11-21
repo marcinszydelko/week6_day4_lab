26dd51a001893a08797bdca48bb0b14b6e5e6651
Marcin <marcinszydelko@yahoo.com>
2019-11-21 16:18:28 +0000

    create list item with book added

--- js/app.js
+++ js/app.js
@@ -9,4 +9,29 @@ const handleFormSubmit = function (event){
   console.log(event.target.title.value);
   console.log(event.target.author.value);
   console.log(event.target.category.value);
+
+  // const newListItem = document.createElement('li');
+  // newListItem.classList.add('book')
+
+
+  const titleListItem = document.createElement('li');
+  const book = document.createElement('article');
+
+  const title = document.createElement('p');
+  title.textContent = `Title: ${event.target.title.value}`;
+  book.appendChild(title);
+
+
+  const author = document.createElement('p');
+  author.textContent = `Author: ${event.target.author.value}`;
+  book.appendChild(author);
+
+  const category = document.createElement('p');
+  category.textContent = `Category: ${event.target.category.value}`;
+  book.appendChild(category);
+
+  titleListItem.appendChild(book);
+
+  const unorderList = document.querySelector('#reading-list');
+  unorderList.appendChild(titleListItem);
 };

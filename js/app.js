44c86ea6c5c508b10a1904ece93ce83065175303
Marcin <marcinszydelko@yahoo.com>
2019-11-21 15:19:57 +0000

    behaviour added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
-})
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit)
+});
+
+const handleFormSubmit = function (event){
+  event.preventDefault();
+  console.log(event.target.title.value);
+  console.log(event.target.author.value);
+  console.log(event.target.category.value);
+};
